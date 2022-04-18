aafa432419a429ac0d2308b4c1e901b6cf90b584
mkhediri <mkhediri@jasmineconseil.com>
2022-04-18 14:26:39 +0100

    add back app

---
+++ src/test/java/com/example/devsecopsback/DevsecopsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devsecopsback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsecopsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
